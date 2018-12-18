acbdc02f9890d4823b6bec27265f0585a3983f0f
SendiSteve <sendi.stev@gmail.com>
2018-12-18 12:01:37 +0300

    Add functionality to add data to database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 const app = express()
 
@@ -17,6 +19,10 @@ app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // load static files
 app.use(express.static('public'))
--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,68 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
+// load note model
+require('../models/Note');
+const Note = mongoose.model('notes');
 
 // Notes route
 router.get('/', (req, res) => {
     res.render('notes/viewNotes');
 });
 
+// Retrieve all notes ideas from the database
+router.get('/notes', (req, res) => {
+    Note.find({})
+    .sort({date: 'desc'})
+    .then(notes => {
+        res.render('notes/viewNotes', {
+            notes:notes
+        });
+    });
+});
+
 // Add notes route
 router.get('/add', (req, res) => {
     res.render('notes/add');
+});
+
+// process the notes form
+router.post('/', (req, res) => {
+    // capture form errors
+    let errors = [];
+    // check if user inputs title
+    if (!req.body.title) {
+        errors.push({
+            text: 'Please add a title for the idea'
+        })
+    }
+    // check if user inputs notes
+    if (!req.body.notes) {
+        errors.push({
+            text: 'Please add notes describing your idea'
+        })
+    }
+    // check if there are any errors
+    if (errors.length > 0) {
+        res.render('notes/add', {
+            errors: errors,
+            title: req.body.title,
+            notes: req.body.notes
+        });
+    } else {
+        // save data to the database
+        const newNote = {
+            title: req.body.title,
+            notes: req.body.notes
+        }
+        new Note(newNote).save()
+            .then(idea => {
+                res.redirect('notes/');
+            })
+
+    }
+
 });
 
 module.exports = router;
